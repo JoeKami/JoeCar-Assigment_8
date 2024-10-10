75223abf5064636340c7118a9372f6dee06bb34f
JoeKami <j.b.carrasquillo@gmail.com>
2024-10-10 16:39:53 -0400

    Created Main and Asynchronous code
    collected data from Output file, used asynchronous code to fetch numbers and organize them into array lists. Then output the frequency of numbers 1-10.

---
+++ src/main/java/com/coderscampus/assignment/Main.java
@@ -0,0 +1,35 @@
+package com.coderscampus.assignment;
+
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Assignment8 assignment = new Assignment8();
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+        List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+            CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(assignment::getNumbers, executor);
+            futures.add(future);
+        }
+
+        List<Integer> allNumbers = futures.stream()
+                .map(CompletableFuture::join)
+                .flatMap(List::stream)
+                .toList();
+
+        Map<Integer, Long> frequencyMap = allNumbers.stream()
+                .collect(Collectors.groupingBy(num -> num, Collectors.counting()));
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + "=" + frequencyMap.getOrDefault(i, 0L) + ", ");
+        }
+
+        executor.shutdown();
+    }
+}
